c5414f4bdb770750acbdf6936ec3fcf96ed330dd
AngelCv22 <angel.cespedesvindas22@gmail.com>
2026-07-13 17:36:48 -0600

    correccion pequena

--- models/ProfesorModel.php
+++ models/ProfesorModel.php
@@ -4,42 +4,36 @@ require_once __DIR__ . '/../config/database.php';
 
 class ProfesorModel
 {
-
-    private PDO $connection;
+    private PDO $conexion;
 
     public function __construct()
     {
-        $this->db = Database::getConnection();
+        $this->conexion = Database::getConnection();
     }
 
     public function getAll()
     {
-
         $sql = "SELECT * FROM profesores
                 WHERE activo = 1";
 
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->conexion->prepare($sql);
 
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
     }
 
     public function getById($id)
     {
-
         $sql = "SELECT *
                 FROM profesores
                 WHERE id = ?
                 AND activo = 1";
 
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->conexion->prepare($sql);
 
         $stmt->execute([$id]);
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
-
     }
-
 }
